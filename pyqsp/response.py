5f99f3d84d35534ab1dd5c76c3456a59761d7952
ichuang <ichuang@mit.edu>
2021-03-27 22:13:10 -0400

    more efficient response computation/plotting code

--- pyqsp/response.py
+++ pyqsp/response.py
@@ -2,7 +2,7 @@ import numpy as np
 import scipy.linalg
 import matplotlib.pyplot as plt
 
-def ComputeQSPResponse(phiset, model="Wx", npts=100, align_first_point_phase=True):
+def ComputeQSPResponse(phiset, model="Wx", npts=100, align_first_point_phase=True, positive_only=False):
     '''
     Compute QSP response.
 
@@ -16,8 +16,13 @@ def ComputeQSPResponse(phiset, model="Wx", npts=100, align_first_point_phase=Tru
       pdat: array of complex-valued U[0,0] values
       model: model
     }
+
+    positive_only: if True, then only use positive a (polynomial argument) values
     '''
-    adat = np.linspace(-1, 1, npts)
+    if positive_only:
+        adat = np.linspace(0, 1, npts)
+    else:
+        adat = np.linspace(-1, 1, npts)
     pdat = []
     sz = np.matrix([[1,0],[0,-1]])
     sx = np.matrix([[0,1],[1,0]])
@@ -29,6 +34,9 @@ def ComputeQSPResponse(phiset, model="Wx", npts=100, align_first_point_phase=Tru
     else:
         raise Exception(f"[PlotQSPRsponse] model={model} unknown - must be Wx (signal is x-rot) or Wz (signal is z-rot)")
     i = (0+1j)
+    pmats = [np.exp(i * phiset[0]) * np.eye(2)]
+    for phi in phiset[1:]:
+        pmats.append( scipy.linalg.expm(i * phi * s_phase))
     for a in adat:
         ao = i * np.sqrt(1-a**2)
         if model in ["Wx", "WxH"]:
@@ -37,9 +45,9 @@ def ComputeQSPResponse(phiset, model="Wx", npts=100, align_first_point_phase=Tru
             W = np.matrix([[a, ao], [ao, a]])
             W = H @ W @ H
             # W = np.matrix([[a, 0], [0, -i * ao]])
-        U = np.exp(i * phiset[0]) * np.eye(2)
-        for phi in phiset[1:]:
-            U = U @ W @ scipy.linalg.expm(i * phi * s_phase)
+        U = pmats[0]
+        for pm in pmats[1:]:
+            U = U @ W @ pm
         if model=="WxH":
             U = H @ U @ H
         pdat.append( U[0,0] )
@@ -55,23 +63,31 @@ def ComputeQSPResponse(phiset, model="Wx", npts=100, align_first_point_phase=Tru
     }
     return ret
 
-def PlotQSPResponse(phiset, model="Wx", npts=100, pcoefs=None, show=True, align_first_point_phase=True):
+def PlotQSPResponse(phiset, model="Wx", npts=100, pcoefs=None, show=True, align_first_point_phase=True,
+                    plot_magnitude=False, plot_positive_only=False):
     '''
     Generate plot of QSP response function polynomial, i.e. Re( <0| U |0> )
     For values of model, see ComputeQSPResponse.
 
     pcoefs - coefficients for expected polynomial response; will be plotted, if provided
     align_first_point_phase - if True, change the complex phase of phase such that the first point has phase angle zero
+    plot_magnitude - if True, show magnitude instead of real and imaginary parts
+    plot_positive_only - if True, then only show positive ordinate values
     '''
-    qspr = ComputeQSPResponse(phiset, model, npts, align_first_point_phase=align_first_point_phase)
+    qspr = ComputeQSPResponse(phiset, model, npts, align_first_point_phase=align_first_point_phase, positive_only=plot_positive_only)
     adat = qspr['adat']
     pdat = qspr['pdat']
 
-    plt.plot(adat, np.real(pdat), 'r')
-    plt.plot(adat, np.imag(pdat), 'g')
+    if plot_magnitude:
+        plt.plot(adat, abs(pdat), 'k')
+        ytext = "magnitude (black)"
+    else:
+        plt.plot(adat, np.real(pdat), 'r')
+        plt.plot(adat, np.imag(pdat), 'g')
+        ytext = "red=real, green=imag"
+
     # plt.plot(adat, abs(pdat), 'k')
 
-    ytext = "red=real, green=imag"
     if pcoefs is not None:
         poly = np.polynomial.Polynomial(pcoefs)
         expected = poly(adat)
