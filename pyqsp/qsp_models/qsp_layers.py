qsp_model needs fix in qsp_layers objective due to factor of i with q(x) in u[1,0]

pyqsp/qsp_models/qsp_layers.py
@@ -73,8 +73,9 @@ class QSP(keras.layers.Layer):
             u = tf.matmul(u, wx)
             u = tf.matmul(u, rz)
 
-        # assume we are interested int he real part of p(x) in the resulting
-        # qsp unitary
+        # assume we are interested in the real part of p(x) and the real part of q(x) in 
+        # the resulting qsp unitary
+        ### FIXME ###
         return tf.math.real(u[:, 0, 0]), tf.math.real(u[:, 0, 1])
 
 