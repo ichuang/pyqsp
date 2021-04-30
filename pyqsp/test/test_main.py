e8f208392fa89e91870743198306603fb49a5565
ichuang <ichuang@mit.edu>
2021-04-30 09:08:49 -0400

    add tf tests to test_main

--- pyqsp/test/test_main.py
+++ pyqsp/test/test_main.py
@@ -1,9 +1,11 @@
+import os
 import unittest
 
 from pyqsp import main
 
 # -----------------------------------------------------------------------------
 # unit tests
+
 test_cmds = [
     "--return-angles --poly=-1,0,2 poly2angles",
     "--return-angles --poly=-1,0,2 --plot --hide-plot poly2angles",
@@ -18,8 +20,29 @@ test_cmds = [
 ]
 
 
+test_cmds_tf = [
+    '--return-angles --func np.cos(3*x) --polydeg 6 polyfunc',
+    '--plot-positive-only --plot --polyargs=19,10 --plot-real-only --polyname poly_sign --method tf poly',
+]
+
 class Test_main(unittest.TestCase):
     def test_main(self):
         for i, cmd in enumerate(test_cmds):
             with self.subTest(i=i):
+                print(f"[pyqsp.test_main testing '{cmd}'")
+                phiset = main.CommandLine(arglist=cmd.split(" "))
+
+    def test_main_tf(self):
+        '''
+        These are slow tests, and run only if PYQSP_TEST_QSP_MODELS is set in the environment
+        '''
+        enabled = 'PYQSP_TEST_QSP_MODELS' in os.environ
+        if not enabled:
+            print(
+                "[pyqsp.test] Skipping qsp_model tests: export PYQSP_TEST_QSP_MODELS=1 to enable these tests")
+            return
+        
+        for i, cmd in enumerate(test_cmds_tf):
+            with self.subTest(i=i):
+                print(f"[pyqsp.test_main testing '{cmd}'")
                 phiset = main.CommandLine(arglist=cmd.split(" "))
